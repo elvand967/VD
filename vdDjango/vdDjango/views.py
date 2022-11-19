6b1fd7633367a93ff41ba0fec569eac2d10e07d3
elvand967 <vladimir.dubanevich@gmail.com>
2022-11-19 23:48:47 +0300

    reversed

--- vdDjango/vdDjango/views.py
+++ vdDjango/vdDjango/views.py
@@ -5,5 +5,10 @@ from django.shortcuts import render
 def about(request):
     return render(request, 'about.html')
 
+def reverse(request):
+    user_text = request.GET["usertext"]
+    reverse = user_text[::-1]
+    return render(request, "reverse.html", {'word': reverse})
+
 def home(request):
     return HttpResponse('This is my home')
